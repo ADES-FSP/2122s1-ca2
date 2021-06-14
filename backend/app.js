e938a6404f8cb6dfdb5629e931ccf33d4f5a425f
Jeremiah Ang <s43500@sp.edu.sg>
2021-06-14 12:09:14 +0800

    Initial

---
+++ backend/app.js
@@ -0,0 +1,12 @@
+// Import required modules
+// e.g. const Ama = require('./logic/ama');
+
+// Create required middleware
+// e.g. app.use(cors());
+// e.g. app.use(express.json());
+
+// Add error handler
+// e.g. app.use(function(err, req, res, next) { ... })
+
+// Start server on port 8080
+// e.g. app.listen(8080, function() { ... });
